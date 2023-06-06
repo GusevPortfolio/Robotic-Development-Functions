bb096dad6629ddc359f58ac6bec5a1aaa40e713b
Maxim Gusev <computer.vision1@outlook.com>
2023-06-06 11:26:04 +0300

    Added perspective change view function

---
+++ Perspective change view.py
@@ -0,0 +1,38 @@
+import matplotlib.image as mplimg
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+#file link and transfer to arrey
+filename = "img01.jpg"
+image = mplimg.imread(filename)
+
+dst_size = 5
+bottom_offset = 6
+
+def perspect_transform(img, src, dst):
+    M = cv2.getPerspectiveTransform(src, dst)
+    warped = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]))
+    return warped
+
+source = np.float32([[14, 140], [301 ,140],[200, 96], [118, 96]])
+
+destination = np.float32([[image.shape[1]/2 - dst_size, image.shape[0] - bottom_offset],
+                  [image.shape[1]/2 + dst_size, image.shape[0] - bottom_offset],
+                  [image.shape[1]/2 + dst_size, image.shape[0] - 2*dst_size - bottom_offset], 
+                  [image.shape[1]/2 - dst_size, image.shape[0] - 2*dst_size - bottom_offset],
+                  ])
+
+warped = perspect_transform(image, source, destination)
+cv2.polylines(image, np.int32([source]), True, (0, 0, 255), 3)
+cv2.polylines(warped, np.int32([destination]), True, (0, 0, 255), 3)
+
+# Output original image and chanhed perspective               
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 6), sharey=True)
+f.tight_layout()
+ax1.imshow(image)
+ax1.set_title('Original Image', fontsize=40)
+ax2.imshow(warped, cmap='gray')
+ax2.set_title('Result', fontsize=40)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+plt.show() 
